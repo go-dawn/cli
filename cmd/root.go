882716618a640433fe9afa2d95c7b5cb32d95b67
kiyon <kiyonlin@163.com>
2020-10-28 15:54:52 +0800

    feat(cli): Colorize long description

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"strings"
+
+	"github.com/muesli/termenv"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +18,7 @@ func init() {
 var rootCmd = &cobra.Command{
 	Use:   "dawn",
 	Short: "Dawn is an opinionated lightweight framework",
-	Long:  longDescription,
+	Long:  colorizeLongDescription(),
 	RunE:  rootRunE,
 }
 
@@ -25,6 +28,34 @@ func rootRunE(cmd *cobra.Command, _ []string) error {
 
 func Run() (err error) {
 	return rootCmd.Execute()
+}
+
+func colorizeLongDescription() string {
+	var oldnew []string
+
+	yellow := `_'/\|,`
+	for _, c := range yellow {
+		logo := termenv.String(string(c)).
+			Foreground(termenv.ANSIYellow).String()
+		oldnew = append(oldnew, string(c), logo)
+	}
+
+	wave := termenv.String("~").
+		Foreground(termenv.ANSICyan).String()
+	oldnew = append(oldnew, "~", wave)
+
+	v := termenv.String(version).
+		Foreground(termenv.ANSIYellow).String()
+	oldnew = append(oldnew, version, v)
+
+	github := termenv.String("https://github.com/go-dawn/dawn").
+		Foreground(termenv.ANSICyan).
+		Underline().Italic().String()
+	oldnew = append(oldnew, "https://github.com/go-dawn/dawn", github)
+
+	replacer := strings.NewReplacer(oldnew...)
+
+	return replacer.Replace(longDescription)
 }
 
 const longDescription = `       __
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/jarcoal/httpmock v1.0.6
+	github.com/muesli/termenv v0.7.4
 	github.com/spf13/cobra v1.1.1
 	github.com/stretchr/testify v1.6.1
 )
